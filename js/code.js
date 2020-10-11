6d4479605756b979b74a572cece6420226a075e9
ZivHoch <71270772+ZivHoch@users.noreply.github.com>
2020-10-11 03:46:24 +0300

    Add files via upload
    fixing the scrollbar code and combining ziv's code with lahav's code

--- js/code.js
+++ js/code.js
@@ -1,52 +1,47 @@
 /***************CODE FUNCTIONS************* */
-(function()
-	{
-		for(var tags = ['main','figure','figcaption'], i = 0; i < tags.length; i ++)
-		{
-			document.createElement(tags[i]);
-		}
-    })();
+(function () {
+	for (var tags = ['main', 'figure', 'figcaption'], i = 0; i < tags.length; i++) {
+		document.createElement(tags[i]);
+	}
+})();
 
-    (function()
-	{
-		if(typeof(window.getComputedStyle) == 'undefined') 
-		{ 
-			return; 
+(function () {
+	if (typeof (window.getComputedStyle) == 'undefined') {
+		return;
+	}
+
+	var pre = document.getElementsByTagName('pre');
+
+	for (var len = pre.length, i = 0; i < len; i++) {
+		var code = pre[i].getElementsByTagName('code').item(0);
+		if (!code) {
+			code = pre[i].getElementsByTagName('samp').item(0);
+			if (!code) {
+				continue;
+			}
 		}
-		
-		var pre = document.getElementsByTagName('pre');
-		
-		for(var len = pre.length, i = 0; i < len; i ++)
-		{
-			var code = pre[i].getElementsByTagName('code').item(0);
-			if(!code)
-			{
-				code = pre[i].getElementsByTagName('samp').item(0);
-				if(!code)
-				{
-					continue;
-				}
-			}
-			
-			var column = document.createElement('div');
-			column.setAttribute('aria-hidden', 'true');
-			
-			for(var n = 0; n < code.innerHTML.split(/[\n\r]/g).length; n ++)
-			{
-				column.appendChild(document.createElement('span'));
-			}
-			
-			pre[i].insertBefore(column, code);
-			pre[i].className = 'line-numbers';
-		}	
-	
-	})();
+
+		var column = document.createElement('div');
+		column.setAttribute('aria-hidden', 'true');
+
+		for (var n = 0; n < code.innerHTML.split(/[\n\r]/g).length; n++) {
+			column.appendChild(document.createElement('span'));
+		}
+
+		pre[i].insertBefore(column, code);
+		pre[i].className = 'line-numbers';
+	}
+
+})();
 
 /**************SIDENAVBAR FUNCTIONS*********** */
 function openNav() {
 	document.getElementById("mySidenav").style.width = "250px";
-  }
-  
-  function closeNav() {
+	document.getElementById("main").style.marginRight = "250px";
+
+}
+
+function closeNav() {
 	document.getElementById("mySidenav").style.width = "0";
-  }
+	document.getElementById("main").style.marginRight = "0";
+}
